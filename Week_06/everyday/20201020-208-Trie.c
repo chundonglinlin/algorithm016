0313bcb898c0e3523c181b194b958a03e6bc4122
chundonglinlin <chundonglinlin@126.com>
2020-10-24 21:20:05 +0800

    add 20201020 daily task

---
+++ Week_06/everyday/20201020-208-Trie.c
@@ -0,0 +1,97 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  20201020-208-Trie.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2020/10/24 21时19分43秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#define MAX_TRIE_COUNT 26
+
+typedef struct {
+    int end;
+    struct _Trie *next[MAX_TRIE_COUNT];
+} Trie;
+
+/** Initialize your data structure here. */
+
+Trie* trieCreate() {
+    return calloc(1, sizeof(Trie));
+}
+
+/** Inserts a word into the trie. */
+void trieInsert(Trie* obj, char * word) {
+    Trie *node = obj;
+
+    for (int i = 0; word[i] != '\0'; i++) {
+        int pos = word[i] - 'a';
+        if ( node->next[pos] == NULL){
+            node->next[pos] = trieCreate();
+        }
+        node = node->next[pos];
+    }
+    node->end = 1;
+}
+
+/** Returns if the word is in the trie. */
+bool trieSearch(Trie* obj, char * word) {
+    Trie *node = obj;
+
+    for (int i = 0; word[i] != '\0'; i++) {
+        int pos = word[i] - 'a';
+        if ( node->next[pos] == NULL){
+            return false;
+        }
+        node = node->next[pos];
+    }
+    return node->end > 0;
+}
+
+/** Returns if there is any word in the trie that starts with the given prefix. */
+bool trieStartsWith(Trie* obj, char * prefix) {
+    Trie *node = obj;
+    for (int i = 0; prefix[i] != '\0'; i++){
+        int pos = prefix[i] - 'a';
+        if ( node->next[pos] == NULL){
+            return false;
+        }
+        node = node->next[pos];
+    }
+    return true;
+}
+
+void trieFree(Trie* obj) {
+    if (obj == NULL) {
+        return;
+    }
+
+    for (int i = 0; i < MAX_TRIE_COUNT; i++) {
+        if (obj->next[i]) {
+            trieFree(obj->next[i]);
+        }
+    }
+
+    free(obj);
+}
+
+/**
+ * Your Trie struct will be instantiated and called as such:
+ * Trie* obj = trieCreate();
+ * trieInsert(obj, word);
+ 
+ * bool param_2 = trieSearch(obj, word);
+ 
+ * bool param_3 = trieStartsWith(obj, prefix);
+ 
+ * trieFree(obj);
+*/
--- Week_06/everyday/README.md
+++ Week_06/everyday/README.md
@@ -145,8 +145,87 @@ int numIslands(char** grid, int gridSize, int* gridColSize){
 ## 2020-10-20
 
 今天临时叫去现场支持，排查hls无发播放问题，ios很神奇，最终发现是推流软件没关闭“消除隔行”，哇哇。。。。。  
-*Day44每日一题推荐：[实现Trie (前缀树）](https://leetcode-cn.com/problems/implement-tri)-prefix-tree/#/description）
+* Day44每日一题推荐：[实现Trie (前缀树）](https://leetcode-cn.com/problems/implement-tri-prefix-tree/#/description）
+* 思路：
 ```bash
+#define MAX_TRIE_COUNT 26
 
+typedef struct {
+    int end;
+    struct _Trie *next[MAX_TRIE_COUNT];
+} Trie;
+
+/** Initialize your data structure here. */
+
+Trie* trieCreate() {
+    return calloc(1, sizeof(Trie));
+}
+
+/** Inserts a word into the trie. */
+void trieInsert(Trie* obj, char * word) {
+    Trie *node = obj;
+
+    for (int i = 0; word[i] != '\0'; i++) {
+        int pos = word[i] - 'a';
+        if ( node->next[pos] == NULL){
+            node->next[pos] = trieCreate();
+        }
+        node = node->next[pos];
+    }
+    node->end = 1;
+}
+
+/** Returns if the word is in the trie. */
+bool trieSearch(Trie* obj, char * word) {
+    Trie *node = obj;
+
+    for (int i = 0; word[i] != '\0'; i++) {
+        int pos = word[i] - 'a';
+        if ( node->next[pos] == NULL){
+            return false;
+        }
+        node = node->next[pos];
+    }
+    return node->end > 0;
+}
+
+/** Returns if there is any word in the trie that starts with the given prefix. */
+bool trieStartsWith(Trie* obj, char * prefix) {
+    Trie *node = obj;
+    for (int i = 0; prefix[i] != '\0'; i++){
+        int pos = prefix[i] - 'a';
+        if ( node->next[pos] == NULL){
+            return false;
+        }
+        node = node->next[pos];
+    }
+    return true;
+}
+
+void trieFree(Trie* obj) {
+    if (obj == NULL) {
+        return;
+    }
+
+    for (int i = 0; i < MAX_TRIE_COUNT; i++) {
+        if (obj->next[i]) {
+            trieFree(obj->next[i]);
+        }
+    }
+
+    free(obj);
+}
+
+/**
+ * Your Trie struct will be instantiated and called as such:
+ * Trie* obj = trieCreate();
+ * trieInsert(obj, word);
+ 
+ * bool param_2 = trieSearch(obj, word);
+ 
+ * bool param_3 = trieStartsWith(obj, prefix);
+ 
+ * trieFree(obj);
+*/
 ```
 
